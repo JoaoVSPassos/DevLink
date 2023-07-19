407c9ea3d77c5184c8f66730006a76a344e55b11
jvspa <jvspassos2@gmail.com>
2023-07-19 17:18:39 -0300

    removed img alt

--- script.js
+++ script.js
@@ -5,16 +5,8 @@ function toggleMode() {
   const img = document.querySelector("#Profile img")
 
   if (html.classList.contains("light")) {
-    img.setAttribute(
-      "alt",
-      "Foto de Mayk Brito sem barba, sorrindo, usando óculos escuros, camisa e jaqueta preta, fundo colorido."
-    )
     img.setAttribute("src", "./assets.avatar-light.png")
   } else {
-    img.setAttribute(
-      "alt",
-      "Foto de Mayk Brito de barba, sorrindo, usando óculos e camisa preta, fundo amarelo."
-    )
     img.setAttribute("src", "./assets avatar.png")
-  }  
+  }
 }
